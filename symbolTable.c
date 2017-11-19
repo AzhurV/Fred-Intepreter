a788a0cc37d6a0796ce0a850162455c5c22648e6
Azhur Victor Viano <avv8047@cs.rit.edu>
2017-11-19 10:04:40 -0500

    Completed basic implementation of the symbol table

--- symbolTable.c
+++ symbolTable.c
@@ -1,3 +1,96 @@
+///file:symbolTable.c
+///description: functions for the symbol table of the Fred interpreter
+///author: avv8047 : Azhur Viano
+
 #include "symbolTable.h"
 
 
+///Create a new empty symbol table
+///@returns a pointer to the new symbol table
+SymbolTable* CreateTable(void){
+  SymbolTable* table = malloc(sizeof(SymbolTable));
+  
+  table->head = NULL;
+  table->size = 0;
+
+  return table;
+}
+
+
+///Add a new symbol to the table
+///@param table the table to add a symbol to
+///@param symbol a pointer to the symbol to add
+///@returns 1 if the symbol was successfully added, 0 if
+///  the symbol already existed in the table
+int AddSymbol(SymbolTable* table, Symbol* symbol){
+  SymbolNode* cur = table->head;
+  SymbolNode* newNode = malloc(sizeof(SymbolNode));
+  newNode->symbol = symbol;
+  int result = 1;
+  
+  if(!cur || (result = strcmp(cur->symbol->name, symbol->name)) >= 0){
+    if(result == 0){
+      free(newNode);
+      return 0;
+    }
+    newNode->next = cur;
+    table->head = newNode;
+    return 1;
+  }
+
+  SymbolNode* prev = cur;
+  cur = cur->next;
+
+  while(cur && (result = strcmp(cur->symbol->name, symbol->name)) < 0){
+    prev = cur;
+    cur = cur->next;
+  }
+
+  if(result == 0){
+    free(newNode);
+    return 0;
+  }
+
+  newNode->next = cur;
+  prev->next = newNode;
+
+  return 1;
+}
+  
+
+///Update a symbol in the table
+///@param table a pointer to the symbol table
+///@param symbol a pointer to the symbol to update, including
+///  the new symbol information
+///@returns 1 if updated successfully, 0 if the symbol was not found
+int updateSymbol(SymbolTable* table, Symbol* symbol){
+  SymbolNode* cur = table->head;
+
+  while(cur){
+    if(strcmp(cur->symbol->name, symbol->name) == 0){
+      free(cur->symbol);
+      cur->symbol = symbol;
+      return 1;
+    }
+    cur = cur->next;
+  }
+
+  return 0;
+}
+
+
+///Retrieve a symbol from the table
+///@param table a pointer to the symbol table
+///@param name the name of the symbol to get
+///@returns a pointer to the symbol if found, or NULL otherwise
+Symbol* GetSymbol(SymbolTable* table, char* name){
+  SymbolNode* cur = table->head;
+  while(cur){
+    if(strcmp(cur->symbol->name, name) == 0){
+      return cur->symbol;
+    }
+    cur = cur->next;
+  }
+
+  return NULL;
+}
--- symbolTable.h
+++ symbolTable.h
@@ -1,30 +1,60 @@
 #ifndef SYM_TABLE_H
 #define SYM_TABLE_H
 
-#include "sorted_list.h"
+#include <stdlib.h>
 
 
+///Node within the symbol table
+typedef struct SymbolNode_ {
+  SymbolNode_* next;
+  Symbol symbol;
+} SymbolNode;
+
+
+///The symbol table
+typedef struct SymbolTable_ {
+  SymbolNode* head;
+  size_t size;
+} SymbolTable;
+
+
+///Types a symbol can have
 typedef enum types_enum {
   Integer, Float, Unknown
 } Type;
 
+
+///Value of a symbol can be either an int or a float
 typedef union val_union {
   int iVal;
   float fVal;
 } Value;
 
 
+///Symbol has a name, a type, and a value
 typedef struct Symbol_ {
   char* name;
   Type type;
-  Value vale;
+  Value value;
 } Symbol;
 
 
-typedef List SymTable;
+///Create a new empty symbol table
+///@returns a pointer to the new symbol table
+SymbolTable* CreateTable(void);
 
 
-void AddSymbol(SymTable, Symbol);
+///Add a new symbol to the table
+///@param table the table to add a symbol to
+///@param symbol a pointer to the symbol to add
+///@returns 1 if the symbol was successfully added, 0 if
+///  the symbol already existed in the table
+void AddSymbol(SymbolTable*, Symbol*);
 
-void GetSymbol(SymTable, char*);
 
+///Update a symbol in the table
+///@param table a pointer to the symbol table
+///@param symbol a pointer to the symbol to update, including
+///  the new symbol information
+///@returns 1 if updated successfully, 0 if the symbol was not found
+Symbol* GetSymbol(SymbolTable*, char*);
